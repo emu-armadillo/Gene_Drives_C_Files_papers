ce2d501ef14af064780b2dd4d64e465496d95500
emu-armadillo <79934154+emu-armadillo@users.noreply.github.com>
2022-01-03 12:31:35 -0800

    Update and rename fig_1.cpp to Data_output_programs/fig_1.cpp

--- Data_output_programs/fig_1.cpp
+++ Data_output_programs/fig_1.cpp
@@ -1,5 +1,4 @@
-// ToUploadGraph_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
